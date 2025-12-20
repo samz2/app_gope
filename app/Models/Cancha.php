de88e09f843a6fc588ed8bd4609a804459eed893
samz2 <antony_1304@hotmail.com>
2025-12-20 11:50:46 -0500

    Initial commit - Laravel project setup

---
+++ app/Models/Cancha.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cancha extends Model
+{
+    public function empresa()
+    {
+        return $this->belongsTo(Empresa::class);
+    }
+}
---
+++ database/migrations/2025_12_20_164007_create_canchas_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('canchas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('empresa_id')->constrained('empresas')->onDelete('cascade');
+            $table->string('nombre');
+            $table->string('tipo')->nullable(); // fútbol 5, 7, 11
+            $table->decimal('precio_hora', 8, 2);
+            $table->boolean('activa')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('canchas');
+    }
+};
